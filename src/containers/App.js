f3471f8a56ce45987930a1d1e9768c65172994a0
rahulhtml <82719075+rahulhtml@users.noreply.github.com>
2021-06-05 22:00:49 +0530

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,77 +7,71 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import "./App.css";
 
 
-class App extends Component{
+class App extends React.Component {
+
+  robotsName;
 
   constructor(){
-    super();
+    super()
     this.state = {
-      robots : [],
-      searchfield : '',
-      popup: false
+      message:'RoboFans',
+      robots:[],
+      searchfield:'',
+      popup:false
     }
-
-    console.log("Constructor!")
   }
 
   onSearchChange = (event) => {
     this.setState({searchfield : event.target.value});
+    console.log(this.state.searchfield);
   }
 
-  async componentDidMount(){
-
-    console.log("Inside will");
-    
-    await fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      return response.json()
+  componentWillMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>{
+        return response.json()
     })
-    .then(users => {
-      this.setState({robots : users});
+    .then(users =>{
+      this.setState({robots:users});
     })
-
-    console.log("After setting state!");
+      this.robotsName = "Sreedhar";
   }
 
-  openPopup = () => {
-    this.setState({popup:true});
+  popItUp = () => {
+    this.setState({popup:true})
   }
 
   closePopup = () => {
-    this.setState({popup:false});
+    this.setState({popup:false})
   }
-
 
   render(){
-    const {robots,searchfield} = this.state;
-    console.log("Render runned");
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
 
-    return !filteredRobots.length ?
-        <h1>Loading.......</h1> 
-        :
-      (
-        <React.Fragment>
-        {this.state.popup ? <Popup closePopup = {this.closePopup}/> : ""}
-        <div className="tc">
-          <h1>Robofans</h1>{/*You need to change this message when no robots name matches! */}
-          <button onClick={this.openPopup}>Open Popup</button>
-          <ErrorBoundary>
-            <SearchBox searchChange = {this.onSearchChange}/>
-          </ErrorBoundary>
-          
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filteredRobots}/>
-            </ErrorBoundary>
-            
-          </Scroll>
-        </div>
-        </React.Fragment>
-      )
+    const filteredRobots = this.state.robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    
+
+
+    return(
+      !filteredRobots.length ?<h1 className="tc">Loding...</h1>
+      :
+      <React.Fragment>
+        {this.state.popup?<Popup closePopup={this.closePopup} name={this.state.robots[0].name} id={this.state.robots[0].id} username={this.state.robots[0].username} email={this.state.robots[0].email} city={this.state.robots[0].address.city}/>:""}
+      <div className="tc">
+        <h1>{this.state.message}</h1>
+        <button onClick={this.popItUp}>PopItUp</button>
+        <SearchBox searchChange = {this.onSearchChange} />
+        <h1>{this.robotsName}</h1>
+        <Scroll>
+          < CardList robots = {filteredRobots}/>
+        </Scroll>
+
+      </div>
+      </React.Fragment>
+    )
   }
+  
 }
 
 export default App;
